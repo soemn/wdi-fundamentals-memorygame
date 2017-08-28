2238e5f666eb4da036bb51ded46056a31a3fd2c9
Unknown <a0086845@u.nus.edu>
2017-08-28 22:43:10 +0800

    unit 8 - card match logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 //console.log("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cardOne);
+
+console.log('user flipped ' + cardOne);
+
+cardsInPlay.push(cardTwo);
+
+console.log('user flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
