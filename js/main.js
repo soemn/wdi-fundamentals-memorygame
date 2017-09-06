663d4bef083fc528b5740f5f4111ae5e75da5177
Unknown <a0086845@u.nus.edu>
2017-09-06 19:44:28 +0800

    unit 11 - dom

--- js/main.js
+++ js/main.js
@@ -1,47 +1,61 @@
 //console.log("Up and running");
 var cards = [
-	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
-	},
-	{
-		rank: "queen",
-		suit: "diamond",
-		cardImage: "images/queen-of-diamond.png"
-	},
-	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
-	},
-	{
-		rank: "king",
-		suit: "diamond",
-		cardImage: "images/king-of-diamond"
-	}
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamond",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king", 
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamond",
+	cardImage: "images/king-of-diamonds.png"
+}
 ];
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
-}
+};
 
-var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
+var flipCard = function () {
+	var cardID = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardID].cardImage);
+	// console.log("User flipped " + cards[cardID].rank);
+	// console.log(cards[cardID].cardImage);
+	// console.log(cards[cardID].suit);
 	cardsInPlay.push(cards[cardID].rank);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
-}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
